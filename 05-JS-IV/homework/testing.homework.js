a459ec499920d334a016da5219f80760058edab1
lucasmartindri <drilucasmartin@gmail.com>
2022-03-11 15:00:04 -0300

    Homework terminada

---
+++ 05-JS-IV/homework/testing.homework.js
@@ -0,0 +1,12 @@
+function nuevoUsuario(nombre, email, password) {
+  // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
+  // Devuelve el objeto
+  // Tu código:
+  var usuario = {
+    nombre: 'nombre',
+    email: 'email',
+    password: 'password',
+  }
+  return usuario;
+}
+console.log(nuevoUsuario('nombre', 'email', 'password'))
